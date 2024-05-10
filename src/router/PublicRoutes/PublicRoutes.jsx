a4c2d1c140ac8842ccb771125a0da8b929ca96b7
ailenad <aialvarez@wecom.global>
2024-05-10 11:57:03 -0300

    Loader login

--- src/router/PublicRoutes/PublicRoutes.jsx
+++ src/router/PublicRoutes/PublicRoutes.jsx
@@ -3,11 +3,11 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginScreen from '../../screens/Login/LoginScreen'
 import SignUpScreen from '../../screens/SignUp/SignUpScreen';
 import { AppContext } from '../../context/AppContext';
-
+import ProgressBar from '../../components/Loader/LoaderLogin';
 const PublicRoutes = () => {
 
   const [ loading,setLoading ] = useState(true);
-
+  const [progress, setProgress] = useState(0);
 
   const { signUpCode,setSignUpCode } = useContext(AppContext);
 
@@ -17,27 +17,43 @@ const PublicRoutes = () => {
 
 
   function getValueLocalStorage () {
+    setProgress(30);
     const codeScreen = localStorage.getItem('code_screen')
     if(codeScreen){
       setSignUpCode(true)
     }else{
       setSignUpCode(false)
     }
+    const interval = setInterval(() => {
+      setProgress(prevProgress => {
+        if (prevProgress >= 100) {
+          clearInterval(interval);
+          setLoading(false);
+          return 100;
+        }
+        return prevProgress + 10; 
+      });
+    }, 250); 
     setTimeout(() => {
+      setProgress(100)
       setLoading(false)      
-    }, 1000);
+    },6000);
   }
   
 
 
   return (
+    
     <Routes>
 
       {
         loading === true ? 
-        <>
-          <Route path='/*' element={<div>Loading...</div>}/>
-        </>
+        <Route path="/*" element={
+          <>
+            <ProgressBar loading={loading} progress={progress} />
+            <div>Loading...</div>
+          </>
+        } />
         :
         <>
           {
