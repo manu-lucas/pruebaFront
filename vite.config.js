b79afc21d65f1b2661f93a67f9d494b1f7412d90
manu-lucas <lmechegaray@wecom.global>
2024-05-16 11:43:46 -0300

    changes in the app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      manifest: {
+        display:"standalone",
+        display_override:[ "window-controls-overlay"],
+        lang: "es-Es",
+        short_name:"ejemplo",
+        description:"Ejemplo de pwa",
+        theme_color :"#19223c",
+        background_color:"#d4d4d4",
+        icons: [
+          {
+            src: 'favicon-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose:"any"
+          },
+          {
+            src: 'favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose:"maskable"
+
+          },
+        ]
+
+      }
+    })
+
+
+  ],
 })
